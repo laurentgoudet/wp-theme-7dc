49b6d14b4cdf946acb9172ff401f8de560df23e5
Laurent Goudet <me@laurentgoudet.com>
2013-03-26 22:34:44 +0000

    Add custom fonts, content.php and update theme

--- footer.php
+++ footer.php
@@ -10,11 +10,13 @@
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
      <?php do_action( '7dc_credits' ); ?>
-     <?php printf(__('%1s %2s by','7dc'), '&copy;', date('Y')); ?> 
-     <a href="<?php echo esc_url( __( 'http://chrisgoudet.com/about/', '7dc' ) ); ?>"title="<?php esc_attr_e( 'About me', '7dc' ); ?>">
-     <?php _e('Chris Goudet', '7dc'); ?></a> - 
-     <a href="<?php echo esc_url( __( 'http://github.com/chrisgoudet/wp-theme-7dc/', '7dc' ) ); ?>"title="<?php esc_attr_e( '7dc theme', '7dc' ); ?>">
-     <?php _e('7dc', '7dc'); ?></a> <?php _e('theme', '7dc'); ?>
+     <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">
+       <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" />
+     </a>
+     <br />
+     <p id="cc-desc">
+      All articles and photos of this blog are licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
+     </p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- header.php
+++ header.php
@@ -29,7 +29,6 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 <header id="masthead" class="site-header" role="banner">
-
     <?php $header_image = get_header_image();
 		if ( ! empty( $header_image ) ) : ?>
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
@@ -37,8 +36,10 @@
 
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
+      <a href="https://twitter.com/chrisgoudet" title="Follow me on Twitter" target="_blank"><i class="icon-twitter"></i></a>
+      <a href="#" title="G+ profile (not yet)"><i class="icon-gplus" target="_blank"></i><span class="screen-reader-text">Google Plus</span></a>
+      <a href="http://chrisgoudet.com/feed" title="RSS is not dead yet!" target="_blank"><i class="icon-rss"></i></a>
+    </hgroup>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h3 class="menu-toggle"><?php _e( 'Menu', '7dc' ); ?></h3>
